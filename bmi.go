ded75955d3046f91600e8405c583c4a2ca5dc59e
Oluwaseyi Daniel <dexter_daniel@outlook.com>
2021-12-12 03:18:43 +0100

    bufferio, conversions, and calculations implemented

--- bmi.go
+++ bmi.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	fmt.Println("BMI Calculator")
+	fmt.Println("------------------------------------------")
+	fmt.Print("Please enter your Weight(kg): ")
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print("Please enter your height(m): ")
+	heightInput, _ := reader.ReadString('\n')
+
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
+	bmi := weight / (height * height)
+
+	fmt.Printf("%.2f\n", bmi)
 }
