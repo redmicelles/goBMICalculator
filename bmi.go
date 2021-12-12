4415d58c0206618eeb46ad0f503d701e59eaa640
Oluwaseyi Daniel <dexter_daniel@outlook.com>
2021-12-12 15:53:33 +0100

    functions refactored

--- bmi.go
+++ bmi.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+
+	"github.com/redmicelles/goBMICalculator/info"
 )
 
-var reader = bufio.NewReader(os.Stdin)
-
 func main() {
-	fmt.Println("BMI Calculator")
-	fmt.Println("------------------------------------------")
-	fmt.Print("Please enter your Weight(kg): ")
-	weightInput, _ := reader.ReadString('\n')
-
-	fmt.Print("Please enter your height(m): ")
-	heightInput, _ := reader.ReadString('\n')
-
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
-
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 64)
+	info.PrintWelcome()
+	weight, height := getUserMetrics()
 
 	bmi := weight / (height * height)
 
